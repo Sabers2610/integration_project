41e3f3a37f02a132b109f7e815d1af99c0e1eded
JuanZweifel <jua.borquez@duocuc.cl>
2024-05-11 16:54:24 -0400

    Modelo detallePedido y arreglo Pedido

---
+++ src/models/DetallePedido.js
@@ -0,0 +1,39 @@
+const { FormatError } = require("../utils/exception.js")
+
+class DetallePedido {
+    constructor(cantidad = 0, sub_total = 0, producto) {
+        this.setCantidad(cantidad)
+        this.setSubTotal(sub_total)
+        this.producto = producto // Clase producto
+    }
+
+    // TODO: Revisar validaciones
+
+    setCantidad(cantidad) {
+        if (cantidad < 0 || cantidad > 999 || typeof cantidad !== "number") {
+            throw new FormatError("Cantidad invalida", "API_FORMAT_ERROR")
+        }
+
+        this.cantidad = cantidad;
+    }
+
+    setSubTotal(sub_total) {
+        if (sub_total < 0 || sub_total > 999999999 || typeof sub_total !== "number") {
+            throw new FormatError("Valor sub total invalido", "API_FORMAT_ERROR")
+        }
+
+        this.sub_total = sub_total;
+    }
+
+    toJson() {
+        const json = {
+            "cantidad": this.cantidad,
+            "sub_total": this.sub_total,
+            "producto": this.producto
+        }
+
+        return json
+    }
+}
+
+module.exports = { DetallePedido }
--- src/models/Pedido.js
+++ src/models/Pedido.js
@@ -1,19 +1,33 @@
+const { FormatError } = require("../utils/exceptions.js")
+
 class Pedido {
-    constructor(rut = "", idPedido = 0, fechaHoraPedido = "", estado = "Pendiente", total = 0) {
-        this.rut = rut
-        this.idPedido = idPedido
-        this.fechaHoraPedido = fechaHoraPedido
+    constructor(id_pedido = 0, cliente = null, productos = null, estado = false, fecha, total) {
+        this.id_pedido = id_pedido
+        this.cliente = cliente // Clase User
+        this.productos = productos // Clase DetallePedido
         this.estado = estado
-        this.total = total
+        this.fecha = fecha
+        this.setTotal(total)
+    }
+
+    // TODO: Revisar validaciones
+
+    setTotal(total) {
+        if (total < 0 || total > 999999999 || typeof total !== "number") {
+            throw new FormatError("Valor total invalido", "API_FORMAT_ERROR")
+        }
+
+        this.total = total;
     }
 
     toJson() {
         const json = {
-            rut: this.rut,
-            idPedido: this.idPedido,
-            fechaHoraPedido: this.fechaHoraPedido,
-            estado: this.estado,
-            total: this.total,
+            "id_pedido": this.id_pedido,
+            "cliente": this.cliente.toJson(),
+            "Productos": this.productos.toJson(),
+            "estado": this.estado ? "Pagado" : "Pendiente",
+            "fecha": this.fecha,
+            "total": this.total,
         }
 
         return json
