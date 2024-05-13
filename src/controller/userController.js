c51bdcf7567697b158947a1f120076aaaa7c416d
JuanZweifel <jua.borquez@duocuc.cl>
2024-05-12 22:04:27 -0400

    Arreglo "exp" de jwt token y creacion middleware
    - Ahora se suma la fecha actual al tiempo de expiracion del token
    - Middleware que verifica el token

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,14 +1,14 @@
-const {User} = require("../models/User.js")
+const { User } = require("../models/User.js")
 const Especiality = require("../models/Especiality.js")
-const {getConnection} = require("../database.js")
+const { getConnection } = require("../database.js")
 const JWT = require("jsonwebtoken")
-const { v4: uuid4} = require("uuid") 
+const { v4: uuid4 } = require("uuid")
 const encript = require("bcryptjs")
 const { SQLError, AuthError, FormatError } = require("../utils/exception.js")
 require("dotenv").config()
 
 User.login = async (request, response) => {
-    const {email, password} = request.body
+    const { email, password } = request.body
     var connection = null
     try {
         connection = await getConnection()
@@ -17,20 +17,21 @@ User.login = async (request, response) => {
                             WHERE correo = ?`
         const values = [email]
         const [rows, fields] = await connection.query(sqlQuery, values)
-        if(rows.length === 0) {
+        if (rows.length === 0) {
             throw new AuthError("User and/or password incorrect", "API_AUTH_UNAUTHORIZED")
         }
         const user = rows[0]
         const passwordValidator = await encript.compare(password, user.clave)
 
-        if(!passwordValidator) throw new AuthError("User and/or password incorrect", "API_AUTH_UNAUTHORIZED")
+        if (!passwordValidator) throw new AuthError("User and/or password incorrect", "API_AUTH_UNAUTHORIZED")
 
-        const tokenPayload ={
+        const tokenPayload = {
             sessionID: uuid4(),
-            user: {name: user.name, email: email},
+            user: { name: user.name, email: email },
             role: user.empleado ? "Empleado" : "Cliente",
             admin: user.admin ? true : false,
-            exp: Number(process.env.JWT_EXPIRE)
+            // Hay que sumar la fecha actual al tiempo de expiracion y dividirla en mil (Unix epoch)
+            exp: Math.floor(Date.now() / 1000) + Number(process.env.JWT_EXPIRE)
         }
 
         const token = JWT.sign(tokenPayload, process.env.JWT_SECRET_KEY, {
@@ -39,13 +40,13 @@ User.login = async (request, response) => {
 
         return response.status(202).json(token)
     } catch (error) {
-        if(!connection) response.status(500).json({
+        if (!connection) response.status(500).json({
             "name": "DATABASE_ERROR",
             "type": "DATABASE_NO_CONNECTION",
             "message": error.message
         })
 
-        if(error instanceof SQLError) return response.status(500).json(error.exceptionJson())
+        if (error instanceof SQLError) return response.status(500).json(error.exceptionJson())
 
         else if (error instanceof AuthError) return response.status(401).json(error.exceptionJson())
 
@@ -56,20 +57,20 @@ User.login = async (request, response) => {
         })
 
     } finally {
-        if(connection) {
+        if (connection) {
             connection.release()
         }
     }
 }
 
 User.register = async (request, response) => {
-    const {rut, name, lastname, age, direction, email, password, esp} = request.body
+    const { rut, name, lastname, age, direction, email, password, esp } = request.body
     var connection = null
     try {
         var passwordHash = await encript.hash(password, 10)
         const user = new User(rut, name, lastname, age, direction, email, passwordHash)
 
-        if(esp !== "") {
+        if (esp !== "") {
             const especiality = new Especiality(esp.id, esp.name_especiality, true)
             user.especiality = especiality
         }
@@ -94,15 +95,15 @@ User.register = async (request, response) => {
 
         return response.status(201).json(user.toJson())
     } catch (error) {
-        if(!connection && !error instanceof FormatError) return response.status(500).json({
+        if (!connection && !error instanceof FormatError) return response.status(500).json({
             "name": "DATABASE_ERROR",
             "type": "DATABASE_NO_CONNECTION",
             "message": error.message
         })
 
-        if(error instanceof SQLError) return response.status(500).json(error.exceptionJson())
+        if (error instanceof SQLError) return response.status(500).json(error.exceptionJson())
 
-        else if(error instanceof FormatError) return response.status(400).json(error.exceptionJson())
+        else if (error instanceof FormatError) return response.status(400).json(error.exceptionJson())
 
         else return response.status(500).json({
             "name": error.name,
@@ -111,9 +112,9 @@ User.register = async (request, response) => {
         })
 
     } finally {
-        if(connection) connection.release()
+        if (connection) connection.release()
     }
-    
+
 }
 
 
--- src/routes/pedidoRoute.js
+++ src/routes/pedidoRoute.js
@@ -1,15 +1,14 @@
 const { Router } = require("express")
+const { authenticateToken } = require("../utils/jwtMiddleware.js")
 const { getPedido, getPedidoId, postPedido } = require("../controller/pedidoController.js")
 
 const router = Router()
 
 router.route("/pedidos/")
 	.get(getPedido)
+	.post(authenticateToken, postPedido)
 
 router.route("/pedidos/:id")
 	.get(getPedidoId)
-
-router.route("/pedidos/")
-	.post(postPedido)
 
 module.exports = router
